dce4eb22347bd93b7d21a44b40146854a7157625
Matthew O'Donnell <matthew.james.od@gmail.com>
2020-07-20 16:39:10 +0100

    LFS test / dialogue tweaks

--- Assets/Scripts/Dialogue/DialogueController.cs
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -131,7 +132,7 @@ public class DialogueController : MonoBehaviour
 
         if(node.Dialogue != null)
         {
-            currentOptions.Clear();
+            currentOptions = currentOptions.Where(x => x.Persist).ToList();
             currentOptions.AddRange(node.Dialogue);
         }
 
